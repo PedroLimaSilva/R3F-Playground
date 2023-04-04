f4cf86f20206888d5b1741cc1a6fa5a246691f72
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2023-04-04 20:22:15 +0100

    Setting up

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import * as THREE from 'three'
+import * as React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Box } from './components/box'
+
+export default function App() {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  )
+}
