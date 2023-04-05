f4045715b68b817c761f4acfaaed1c7e4c33adc1
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2023-04-05 12:05:03 +0000

    Update 4 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
 /* eslint-disable */
-import * as THREE from 'three';
-import * as React from 'react';
-import { Canvas } from '@react-three/fiber';
-import { Experience } from './Experience';
+import * as THREE from "three";
+import * as React from "react";
+import { Experience } from "./Experience";
 
 export default function App() {
-  return (
-    <Canvas>
-      <Experience />
-      <OrthographicCamera
-        makeDefault
-        zoom={1}
-        top={200}
-        bottom={-200}
-        left={200}
-        right={-200}
-        near={1}
-        far={2000}
-        position={[0, 0, 200]}
-      />
-    </Canvas>
-  );
+  return <Experience />;
 }
--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,22 +1,35 @@
-import { OrbitControls } from '@react-three/drei';
-import { Perf } from 'r3f-perf';
-import * as React from 'react';
-import { Box } from './components/box';
-import { Room } from './components/Room';
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import * as React from "react";
+
+import { Box } from "./components/box";
+import { Room } from "./components/Room";
 
 export function Experience() {
   return (
-    <>
-      <Perf position='top-left' />
+    <Canvas
+      orthographic
+      camera={{
+        fov: 45,
+        near: 0.01,
+        far: 200,
+        zoom: 100,
+        position: [0, 0, 100],
+      }}
+    >
+      <Perf position="top-left" />
 
       <OrbitControls
         makeDefault
-        maxDistance={15}
-        minDistance={5}
-        enablePan={false}
-        maxPolarAngle={Math.PI / 2}
-        maxAzimuthAngle={Math.PI / 4}
-        minAzimuthAngle={-Math.PI / 4}
+        // maxZoom={200}
+        // minZoom={75}
+        // maxDistance={1}
+        // minDistance={1}
+        // enablePan={false}
+        // maxPolarAngle={Math.PI / 2}
+        // maxAzimuthAngle={Math.PI / 4}
+        // minAzimuthAngle={-Math.PI / 4}
       />
 
       <ambientLight intensity={0.3} />
@@ -28,9 +41,8 @@ export function Experience() {
         position={[100, 100, 100]}
       />
 
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <Box position={[0, 0, 0]} />
       <Room />
-    </>
+    </Canvas>
   );
 }
