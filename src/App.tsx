458809ac1ad59124fa73bd25e7a95a982e8f3577
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2023-04-05 15:04:34 +0000

    Fix formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable */
-import * as THREE from "three";
-import * as React from "react";
-import { Experience } from "./Experience";
+import * as THREE from 'three';
+import * as React from 'react';
+import { Experience } from './Experience';
 
 export default function App() {
   return <Experience />;
